a378b625b2f565b40fa2e18560ec6ca7988b4f6d
premdev <prempravash65@gmail.com>
2026-06-18 13:49:18 +0530

    NoPage Found added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import NoPage from "./Pages/NoPage";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>} ></Route>
+         <Route path="/about" element={<About></About>} ></Route>
+          <Route path="/contact" element={<Contact></Contact>} ></Route>
+          <Route path="*" element={<NoPage></NoPage>} ></Route>
 
-export default App
+      </Routes>
+    </Router>
+  </>;
+};
+
+export default App;
---
+++ src/Pages/NoPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NoPage.css"
+
+const NoPage = () => {
+  return (
+    <div className="nopage-container">
+      <h1 className="h1" >The page you’re looking for</h1>
+      <h1 className="h1" > can’t be found.</h1>
+    </div>
+  );
+};
+
+export default NoPage;
